0bee09c267f7fa9e6baf16b462f49852a5fab1c7
Brandon Bay <brandon.cuyler.bay@gmail.com>
2018-01-31 17:48:38 -0600

    Added sprites, movement scripts
    Sprite "square"'
    Script "Player_script.cs" inherits "Entity_script.cs" which defines
    movemnt.

---
+++ BW/Assets/Scripts/Entity_script.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity_script : MonoBehaviour {
+
+    /// <summary>
+    /// Movement speed
+    /// </summary>
+    public float speed;
+
+    /// <summary>
+    /// Future position
+    /// </summary>
+    Vector3 pos;
+
+    // <summary>
+    /// Player controls
+    /// </summary>
+    public KeyCode UpKey, DownKey, LeftKey, RightKey;
+
+    protected void Move()
+    {
+        bool up = Input.GetKey(UpKey);
+        bool down = Input.GetKey(DownKey);
+        bool left = Input.GetKey(LeftKey);
+        bool right = Input.GetKey(RightKey);
+
+        if (transform.position == pos)
+        {
+            if (up)
+                pos += Vector3.up;
+            else if (down)
+                pos += Vector3.down;
+            else if (left)
+                pos += Vector3.left;
+            else if (right)
+                pos += Vector3.right;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+        pos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Move
+        Move();
+    }
+
+}
