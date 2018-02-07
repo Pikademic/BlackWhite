b61fe0b73db84f1aee0c916bd7565c694b1c9b11
Brandon Bay <brandon.cuyler.bay@gmail.com>
2018-02-07 14:12:24 -0600

    Added tilemap, including collision and "painting"
    TileMap is composed of square_white and square_black assets. Entities
    have public "mytile" fields. Entities place "mytile" into their current
    position when moving, and cannot move into squares of "mytile."

--- BW/Assets/Scripts/Entity_script.cs
+++ BW/Assets/Scripts/Entity_script.cs
@@ -10,6 +10,16 @@ public class Entity_script : MonoBehaviour {
     public float speed;
 
     /// <summary>
+    /// Tilemap
+    /// </summary>
+    public UnityEngine.Tilemaps.Tilemap tm;
+
+    /// <summary>
+    /// Tile that entity "paints"
+    /// </summary>
+    public UnityEngine.Tilemaps.Tile mytile;
+
+    /// <summary>
     /// Future position
     /// </summary>
     Vector3 pos;
@@ -17,7 +27,12 @@ public class Entity_script : MonoBehaviour {
 
     protected void Move(Vector3 rel)
     {
-        if (transform.position == pos) pos += rel;
+        if (transform.position == pos) // if entity has finished its previous move
+            if (tm.GetTile(Vector3Int.FloorToInt(pos + rel)) != mytile) // if entity is not attempting to move to a tile of its own color
+            {
+                tm.SetTile(Vector3Int.FloorToInt(pos), mytile); // place the entity's tile in its current position
+                pos += rel; // set destination
+            }
     }
 
     // Use this for initialization
@@ -25,6 +40,8 @@ public class Entity_script : MonoBehaviour {
     {
 
         pos = transform.position;
+        tm = FindObjectOfType<UnityEngine.Tilemaps.Tilemap>();
+        Debug.Log(tm);
 
     }
 
